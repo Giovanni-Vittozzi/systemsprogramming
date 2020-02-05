fc5a42c19db7b18fa05c547fc5bf5aab9aa292d5
Giovanni-Vittozzi <gvittozzi1@pride.hofstra.edu>
2020-02-05 14:53:29 -0500

    2_5_2020 pre-class

--- asnmt1/lnkdlst.c
+++ asnmt1/lnkdlst.c
@@ -16,12 +16,41 @@ void Add_At_End(NODE *head, int num)
     {
         current = current->next;
     } //check to make sure at the end
-    current->next= (NODE *)malloc(sizeof(NODE));
+    current->next = (NODE *)malloc(sizeof(NODE));
     current->data = num;
-    current->next=NULL;
+    current->next = NULL;
 
     printf("%d\n", current->data);
 }
+
+// int Delete_Node(NODE *head, int num)
+// {
+//     //1)Go to node we want to delete
+//     //2)Save the node we want to delete in a temp pointer
+//     //3)Set previous node's next pointer to point toh the node we wish to delete
+//     //4)Delete node using the temp pointer
+
+//     int i = 0;
+//     int retval = -1;
+//     NODE *current = head;
+//     NODE *temp = NULL;
+//     if (num == 0)
+//     {
+//         return head->data;
+//     }
+//        for (i = 0; i < num-1; i++) {
+//         if (current->next == NULL) {
+//             return -1;
+//         }
+//         current = current->next;
+//     }
+//     temp=current->next;
+//     retval=temp->data;
+//     current->next=temp->next;
+//     free(temp);
+//     return retval;
+
+// }
 
 int main(int argc, char *argv[])
 {
@@ -30,7 +59,6 @@ int main(int argc, char *argv[])
     head = (NODE *)malloc(sizeof(NODE));
     head->data = 3;
     head->next = NULL;
-
     printf("%d\n", head->data);
 
     head->next = (NODE *)malloc(sizeof(NODE));
@@ -42,9 +70,11 @@ int main(int argc, char *argv[])
 
     printf("%d\n", current->data);
 
-    
     Add_At_End(head, 8);
     Add_At_End(head, 9);
+    Add_At_End(head, 10);
+    Add_At_End(head, 12);
+    //Delete_Node(head,2);
     free(current);
     free(head);
 }

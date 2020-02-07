0c0803e4aa7b80b35c551bbf315a3c9cdca6a0d8
Giovanni-Vittozzi <gvittozzi1@pride.hofstra.edu>
2020-02-07 14:04:26 -0500

    2_7_2020 pre-class fixed Add_At_End in printlsttest.c

--- asnmt1/printlsttest.c
+++ asnmt1/printlsttest.c
@@ -11,40 +11,60 @@ typedef struct Node
 
 void Add_At_End(NODE *head, int num)
 {
-    NODE *current = head;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    } //check to make sure at the end
-    // printf("current = %d\n", current->data);
-    current->next = (NODE *)malloc(sizeof(NODE));
-    current->data = num;
-    current->next->next = NULL;
+    NODE *nodeNew, *save;
 
-    printf("%d\n", current->data);
+    nodeNew = (NODE *)malloc(sizeof(NODE));
+    nodeNew->data = num;  //sets up new node and adds correct data
+    nodeNew->next = NULL; //creates next pointer which goes to NULL
+
+    save = head; //temporary pointer used to put this new node at the end of the list
+
+    while (save->next != NULL) //moves to the end of the list
+    {
+        save = save->next;
+        // printf("current here is: %d\n", *save);
+    }
+    save->next = nodeNew;          //adds node to the end of the list
+    printf("%d\n", nodeNew->data); //prints value that was added
+
+    // NODE *current = head; ///This way of doing it kept moving the head pointer up
+    // printf("head here is: %d\n", *head);
+    // while (current->next != NULL)
+    // {
+    //     current = current->next;
+    //     printf("current here is: %d\n", *current);
+
+    // } //check to make sure at the end
+    // // printf("current = %d\n", current->data);
+    // current->next = (NODE *)malloc(sizeof(NODE));
+    // current->data = num;
+    // current->next->next = NULL;
+
+    // printf("%d\n", current->data);
+
+    // printf("head at end of add is: %d\n", *head);
 }
 
-void print_list(NODE *head)//prints one forward
+void print_list(NODE *head) //prints one forward
 {
     NODE *temp;
     temp = head;
     printf("Print function is printing list: \n");
-    while(temp != NULL){
+    while (temp != NULL)
+    {
         printf("%d\n", temp->data);
         temp = temp->next;
     }
-
 }
 
 int main(int argc, char *argv[])
 {
-    NODE *head, *current;
+    NODE *head, *current = NULL;
 
     head = (NODE *)malloc(sizeof(NODE));
     head->data = 3;
     head->next = NULL;
     printf("%d\n", head->data);
-    current = (NODE *)malloc(sizeof(NODE));
 
     Add_At_End(head, 1);
     Add_At_End(head, 2);
@@ -52,6 +72,5 @@ int main(int argc, char *argv[])
     Add_At_End(head, 14);
 
     print_list(head);
-    free(current);
     free(head);
 }

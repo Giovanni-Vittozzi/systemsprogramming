943fd7332adb4a6bd66a61445134448ae0b9cf8b
Giovanni-Vittozzi <gvittozzi1@pride.hofstra.edu>
2020-02-11 16:42:48 -0500

    2_11_2020 fixed insert_node function to accomodate putting before head and added more comments

--- asnmt1/lnkdlst.c
+++ asnmt1/lnkdlst.c
@@ -13,7 +13,9 @@ typedef struct Node
 
 } NODE;
 
-void Add_At_End(NODE *head, int num);   // function adds new node at end of linked list
+// print node pass reference to node to be printed like print n times
+void Add_At_End(NODE *head, int num);                 // function adds new node at end of linked list
+NODE *Insert_Node(NODE *head, int num, int putbefore); // function adds new node at end of linked list
 NODE *findNode(NODE *head, int value);  //function finds nodebased on given value and returns address of that node
 NODE *deleteNode(NODE *head, int data); //function deletes node based on given value and uses findeNode to do so
 void print_list(NODE *head);            //function prints the entire list
@@ -31,7 +33,8 @@ int main(int argc, char *argv[])
     Add_At_End(head, 2);
     Add_At_End(head, 10);
     Add_At_End(head, 14);
-
+    Add_At_End(head, 15);
+    Add_At_End(head, 17);
     print_list(head);
 
     head = deleteNode(head, 3);
@@ -40,6 +43,12 @@ int main(int argc, char *argv[])
     head = deleteNode(head, 10);
     print_list(head);
 
+    head = Insert_Node(head, 12, 14);
+    print_list(head);
+    head = Insert_Node(head, 16, 17);
+    print_list(head);
+    head = Insert_Node(head, 0, 1);
+    print_list(head);
     free(head);
 }
 
@@ -70,6 +79,49 @@ void Add_At_End(NODE *head, int num)
     printf("Node added: %d\n", nodeNew->data); //prints value that was added
 }
 
+NODE *Insert_Node(NODE *head, int num, int putbefore)
+{
+    printf("Insert_Node is inserting %d before %d\n", num, putbefore);
+    // NODE *tempy = NULL; //temporary pointer to head
+    NODE *tempy = head; //temporary pointer to head
+    NODE *newNode;      //new node to be added before our value
+    newNode = (NODE *)malloc(sizeof(NODE));
+
+    NODE *addr_putbefore = findNode(head, putbefore);
+
+    if (head != NULL) //checks if list is empty
+    {
+        if (head->data == putbefore)
+        {
+            newNode->data = num;
+            newNode->next = tempy;
+            head = newNode;
+            return head;
+        }
+        else
+        {
+            while (tempy->data != putbefore) //loop to move pointer before the value we're searching for
+            {
+                // printf("tempy: %d\n", *tempy); //temporary print statement for debugging
+                if (tempy->next == addr_putbefore)
+                {
+                    // tempy2 = tempy->next;
+                    // printf("tempy2: %d\n", *tempy2); //temporary print statement for debugging
+
+                    break;
+                }
+                tempy = tempy->next; //
+            }
+            tempy->next = newNode;
+            newNode->data = num;
+            // printf("addr_putbefore is: %d\n", *addr_putbefore); //temporary debugging statement
+            newNode->next = addr_putbefore;
+            return head;
+        }
+        printf("Node inserted\n");
+    }
+}
+
 NODE *findNode(NODE *head, int value) //find function returns pointer to the given node value
 {
     NODE *current = NULL; //current pointer that we're initializing to 0
@@ -78,13 +130,13 @@ NODE *findNode(NODE *head, int value) //find function returns pointer to the giv
     // printf("Value: %d\n", value);
     if (head != NULL) //checks if list is empty
     {
-        while (current->next != NULL) //moves through the linked list to the end
+        while (current != NULL) //moves through the linked list to the end
+        //NOTE: did this instead of doing current->next != NULL (this doesn't do the last element)
         {
 
             if (current->data == value) //if the data at current node is equal to the searched for value, return 1
             {
                 found = 1;
-                // printf("Found %d", found);
                 break;
             }
             current = current->next; //if it's not equal then move to the next node
@@ -107,8 +159,8 @@ NODE *deleteNode(NODE *head, int value)
         head = current->next; //move the head forward
         printf("head here is %d\n", *head);
         current->next = NULL; //Delete the old head
-        free(current); //pretty sure I have to do this here
-        return head; //return the new head
+        free(current);        //pretty sure I have to do this here
+        return head;          //return the new head
     }
     else //if we arent deleting the head
     {
